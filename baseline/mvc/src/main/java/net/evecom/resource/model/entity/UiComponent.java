d8f7d9c5cb8707f30fe4d178f26320e294ee3931
newmum <357391549@qq.com>
2018-10-26 09:21:17 +0800

    11

---
+++ baseline/mvc/src/main/java/net/evecom/resource/model/entity/UiComponent.java
@@ -0,0 +1,151 @@
+package net.evecom.resource.model.entity;
+
+import net.evecom.core.db.model.entity.DataEntity;
+import net.evecom.utils.verify.CheckUtil;
+import io.swagger.annotations.ApiModelProperty;
+import org.beetl.sql.core.annotatoin.Table;
+
+import javax.persistence.Column;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author xiejun
+ * @since 1.0
+ */
+@Table(name = "ui_component")
+public class UiComponent extends DataEntity<UiComponent> implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Column(name = "parent_id")
+	@ApiModelProperty(value = "父类id")
+	private Long parentId;
+	@Column(name = "name")
+	@ApiModelProperty(value = "名称")
+	private String name;
+	@Column(name = "title")
+	@ApiModelProperty(value = "标题")
+	private String title;
+	@Column(name = "element_id")
+	@ApiModelProperty(value = "元素id")
+	private Long elementId;
+	@Column(name = "sort")
+	@ApiModelProperty(value = "排序,正序 越大排越后 ")
+	private Long sort;
+	@Column(name = "is_use")
+	@ApiModelProperty(value = "是否使用(1使用2不使用)")
+	private int isUse;
+	@Column(name = "crm_power_id")
+	@ApiModelProperty(value = "权限id")
+	private Long crmPowerId;
+	@Column(name = "ui_router_id")
+	@ApiModelProperty(value = "菜单id")
+	private Long uiRouterId;
+	@ApiModelProperty(value = "子类组件集合", hidden = true)
+	private List<UiComponent> childList;
+	@ApiModelProperty(value = "组件属性集合", hidden = true)
+	private List<UiComEle> elelist;
+
+	public void addEle(UiComEle comEle) {
+		if (CheckUtil.isNull(elelist)) {
+			elelist = new ArrayList<UiComEle>();
+		}
+		elelist.add(comEle);
+	}
+
+	public List<UiComEle> getElelist() {
+		if (CheckUtil.isNull(elelist)) {
+			elelist = new ArrayList<UiComEle>();
+		}
+		return elelist;
+	}
+
+	public void setElelist(List<UiComEle> elelist) {
+		this.elelist = elelist;
+	}
+
+	public List<UiComponent> getChildList() {
+		if (CheckUtil.isNull(childList)) {
+			childList = new ArrayList<UiComponent>();
+		}
+		return childList;
+	}
+
+	public void setChildList(List<UiComponent> childList) {
+		this.childList = childList;
+	}
+
+	public Long getParentId() {
+		return parentId;
+	}
+
+	public void setParentId(Long parentId) {
+		this.parentId = parentId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Long getElementId() {
+		return elementId;
+	}
+
+	public void setElementId(Long elementId) {
+		this.elementId = elementId;
+	}
+
+	public Long getSort() {
+		return sort;
+	}
+
+	public void setSort(Long sort) {
+		this.sort = sort;
+	}
+
+	public int getIsUse() {
+		return isUse;
+	}
+
+	public void setIsUse(int isUse) {
+		this.isUse = isUse;
+	}
+
+	public Long getCrmPowerId() {
+		return crmPowerId;
+	}
+
+	public void setCrmPowerId(Long crmPowerId) {
+		this.crmPowerId = crmPowerId;
+	}
+
+	public Long getUiRouterId() {
+		return uiRouterId;
+	}
+
+	public void setUiRouterId(Long uiRouterId) {
+		this.uiRouterId = uiRouterId;
+	}
+
+	@Override
+	public String toString() {
+		return "UiComponent [" + "parentId=" + parentId + ",name=" + name + ",title=" + title + ",elementId="
+				+ elementId + ",sort=" + sort + ",isUse=" + isUse + ",crmPowerId=" + crmPowerId + ",uiRouterId="
+				+ uiRouterId + "]" + "Address [" + getClass().getName() + "@" + Integer.toHexString(hashCode()) + "]";
+	}
+
+}
---
+++ baseline/mvc/src/main/java/net/evecom/resource/model/service/UiComponentService.java
@@ -0,0 +1,148 @@
+package net.evecom.resource.model.service;
+
+import net.evecom.core.rbac.base.BaseService;
+import net.evecom.resource.model.dao.IUiComponentDao;
+import net.evecom.core.rbac.model.entity.CrmPower;
+import net.evecom.core.db.model.entity.Resources;
+import net.evecom.resource.model.entity.UiComEle;
+import net.evecom.resource.model.entity.UiComponent;
+import net.evecom.resource.model.entity.UiComponentProp;
+import net.evecom.resource.model.entity.UiElement;
+import net.evecom.core.db.model.service.ResourceService;
+import net.evecom.tools.constant.consts.SqlConst;
+import net.evecom.core.db.database.query.QueryParam;
+import net.evecom.tools.service.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author xiejun
+ */
+
+@Service("uiComponentService")
+public class UiComponentService extends BaseService {
+    @Resource
+    private ResourceService resourceService;
+    @Resource
+    private IUiComponentDao iUiComponentDao;
+
+    public List<UiComponent> getComponents(List<CrmPower> list, Long routerId) throws Exception {
+        StringBuffer sb = new StringBuffer();
+        for (CrmPower power : list) {
+            sb.append(power.getId().toString() + ",");
+        }
+        String value = sb.toString().substring(0, sb.length() - 1);
+        QueryParam<UiComponent> param = new QueryParam<>();
+        param.append(UiComponent::getCrmPowerId, value, SqlConst.IN, SqlConst.AND);
+        param.append(UiComponent::getUiRouterId, routerId);
+        // 获取权限下的所有组件集合
+        List<UiComponent> componentList = (List<UiComponent>) resourceService.list(UiComponent.class, param).getList();
+        if (componentList == null || componentList.size() == 0) {
+            return componentList;
+        }
+        for (UiComponent uiComponent : componentList) {
+            if (uiComponent.getName().equals("table")||uiComponent.getName().equals("form")) {
+                List<UiComponent> childList = iUiComponentDao.getChildListById(uiComponent.getId());
+                uiComponent.setChildList(childList);
+            }
+        }
+        sb = new StringBuffer();
+        Map<Long, UiComponent> map = new HashMap<Long, UiComponent>();
+        for (UiComponent uiComponent : componentList) {
+            map.put(uiComponent.getId(), uiComponent);
+            sb.append(uiComponent.getId().toString() + ",");
+            if (uiComponent.getChildList().size() > 0) {
+                for (UiComponent temp : uiComponent.getChildList()) {
+                    map.put(temp.getId(), temp);
+                    sb.append(temp.getId().toString() + ",");
+                }
+            }
+        }
+        // 通过组件id获取组件的属性与值
+        param.clear();
+        value = sb.toString().substring(0, sb.length() - 1);
+        param.append("component_id", value, SqlConst.IN, SqlConst.AND);
+        Resources resources = resourceService.get("ui_component_element_view");
+        List<UiComEle> comEleList = (List<UiComEle>) resourceService.list(resources, param).getList();
+        for (UiComEle uiComEle : comEleList) {
+            long componentId = uiComEle.getComponentId();
+            UiComponent temp = map.get(componentId);
+            if (temp != null) {
+                map.get(componentId).addEle(uiComEle);
+            }
+        }
+
+        return componentList;
+    }
+
+    public void addCheck(UiComponent uiComponent) throws Exception {
+    }
+
+    @Transactional
+    public void add(UiComponent uiComponent) throws Exception {
+        resourceService.add(uiComponent);
+        addUiProp(uiComponent);
+    }
+
+    @Transactional
+    public void addUiProp(UiComponent uiComponent) throws Exception {
+        QueryParam<UiElement> param = new QueryParam<UiElement>();
+        param.append(UiElement::getParentId, uiComponent.getElementId());
+        Page<UiElement> page = (Page<UiElement>) resourceService.list(UiElement.class, param);
+        boolean bo = (page != null && page.getList() != null && page.getList().size() > 0);
+        //为组件添加所有元素属性,并设置最初默认值
+        if (bo) for (UiElement uiElement : page.getList()) {
+            UiComponentProp prop = new UiComponentProp();
+            prop.setComponentId(uiComponent.getId());
+            prop.setElementId(uiElement.getId());
+            prop.setPropValue(uiElement.getValue());
+            prop.setIsUse(1);
+            resourceService.add(prop);
+        }
+    }
+
+    public void editCheck(UiComponent uiComponent) throws Exception {
+    }
+
+    @Transactional
+    public void update(UiComponent uiComponent) throws Exception {
+        UiComponent old_uiComponent = (UiComponent) resourceService.get(UiComponent.class, uiComponent.getId());
+        if (!old_uiComponent.getElementId().equals(uiComponent.getElementId())) {
+            QueryParam<UiComponentProp> param = new QueryParam<UiComponentProp>();
+            param.append(UiComponentProp::getComponentId, uiComponent.getId());
+            resourceService.delete(UiComponentProp.class, param);
+            addUiProp(uiComponent);
+        }
+        resourceService.update(uiComponent);
+    }
+
+    public List<Long> delete(Long id) throws Exception {
+        List<UiComponent> list = getChildFile(id);
+        Long[] ids = new Long[list.size() + 1];
+        for (int i = 0; i < list.size(); i++) {
+            ids[i] = list.get(i).getId();
+        }
+        ids[ids.length - 1] = id;
+        Resources resource = resourceService.get("ui_component");
+        return resourceService.delete(resource, ids);
+    }
+
+    public List<Long> delete(Long... ids) throws Exception {
+        List<Long> result=new ArrayList<Long>();
+        for (Long id : ids) {
+            result.addAll(delete(id));
+        }
+        return null;
+    }
+
+    public List<UiComponent> getChildFile(Long componentId) {
+        List<UiComponent> fileChildList = iUiComponentDao.getChildListById(componentId);
+        return fileChildList;
+    }
+}
