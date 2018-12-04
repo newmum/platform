b5d7f99ef01c65321f27d8ad816b932ddd18b504
newmum <357391549@qq.com>
2018-12-04 11:17:59 +0800

    11

---
+++ baseline/resource/src/main/java/net/evecom/rd/ie/baseline/core/db/config/DBPoolConfig.java
@@ -0,0 +1,49 @@
+package net.evecom.rd.ie.baseline.core.db.config;
+
+import net.evecom.rd.ie.baseline.core.db.database.pool.DruidFilter;
+import net.evecom.rd.ie.baseline.core.db.database.pool.DruidServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description: Druid监控web配置
+ * @author： zhengc
+ * @date： 2018年5月17日
+ */
+@Configuration
+public class DBPoolConfig {
+	private static final Logger log = LoggerFactory.getLogger(DBPoolConfig.class);
+
+	/**
+	 * 配置监控界面servlet
+	 *
+	 * @return
+	 */
+	@Bean
+	public ServletRegistrationBean<DruidServlet> druidServlet() {
+		log.info("rest druidServlet init");
+		ServletRegistrationBean<DruidServlet> reg = new ServletRegistrationBean<DruidServlet>();
+		reg.setServlet(new DruidServlet());
+		reg.addUrlMappings("/druid/*");
+		return reg;
+	}
+
+	/**
+	 * 配置监控界面过滤器
+	 *
+	 * @return
+	 */
+	@Bean
+	public FilterRegistrationBean<DruidFilter> filterRegistrationBean() {
+		log.info("rest filterRegistrationBean init");
+		FilterRegistrationBean<DruidFilter> filterRegistrationBean = new FilterRegistrationBean<DruidFilter>();
+		filterRegistrationBean.setFilter(new DruidFilter());
+		filterRegistrationBean.addUrlPatterns("/*");
+		return filterRegistrationBean;
+	}
+
+}
---
+++ baseline/resource/src/main/java/net/evecom/rd/ie/baseline/core/db/config/RedisConfig.java
@@ -0,0 +1,31 @@
+package net.evecom.rd.ie.baseline.core.db.config;
+
+import net.evecom.rd.ie.baseline.utils.database.redis.JedisConfig;
+import net.evecom.rd.ie.baseline.utils.database.redis.RedisClient;
+import net.evecom.rd.ie.baseline.utils.file.PropertiesUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description:
+ * @author： zhengc
+ * @date： 2018年5月30日17:13:38
+ */
+@Configuration
+public class RedisConfig {
+
+    @Bean(name = "redisClient", value = "redisClient")
+    public RedisClient redisTool() {
+        PropertiesUtils global = new PropertiesUtils(RedisConfig.class.getClassLoader().getResourceAsStream("redis.properties"));
+        JedisConfig config = new JedisConfig();
+        config.setIp(global.getKey("redis.ip"));
+        config.setPost(Integer.valueOf(global.getKey("redis.post")));
+        config.setMaxIdle(Integer.valueOf(global.getKey("redis.maxidle")));
+        config.setMaxWait(Integer.valueOf(global.getKey("redis.maxwait")));
+        config.setTestOnBorrow(Boolean.valueOf(global.getKey("redis.testonborrow")));
+        config.setMaxActive(Integer.valueOf(global.getKey("redis.maxactive")));
+        config.setPassword(global.getKey("redis.password"));
+        RedisClient redisClient = new RedisClient(config);
+        return redisClient;
+    }
+}
