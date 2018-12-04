4b2e7d29e749e15bcf492cd4c553370e024e9a2a
newmum <357391549@qq.com>
2018-12-04 11:20:05 +0800

    11

---
+++ baseline/common/src/main/java/net/evecom/rd/ie/baseline/utils/iterable/ListUtil.java
@@ -0,0 +1,28 @@
+package net.evecom.rd.ie.baseline.utils.iterable;
+
+import java.util.List;
+
+/**
+ * @ClassName: ListUtil
+ * @Description: List集合组件
+ * @author： zhengc
+ * @date： 2016年6月3日
+ */
+public class ListUtil {
+
+    /**
+     * list 数据去重
+     * @param list
+     * @return
+     */
+    public static List<?> removeDuplicate(List<?> list) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = list.size() - 1; j > i; j--) {
+                if (list.get(j).equals(list.get(i))) {
+                    list.remove(j);
+                }
+            }
+        }
+        return list;
+    }
+}
