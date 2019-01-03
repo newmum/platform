cbca76966a6e1b79e9ace06a632282c4aa31e6c0
newmum <357391549@qq.com>
2019-01-03 09:45:20 +0800

    11

--- project/weather-etl-processor/src/main/java/net/evecom/rd/ie/baseline/etl/processor/controller/ProcessController.java
+++ project/weather-etl-processor/src/main/java/net/evecom/rd/ie/baseline/etl/processor/controller/ProcessController.java
@@ -2,17 +2,25 @@ package net.evecom.rd.ie.baseline.etl.processor.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import net.evecom.rd.ie.baseline.core.db.model.entity.Resources;
+import net.evecom.rd.ie.baseline.core.db.model.service.ResourceService;
 import net.evecom.rd.ie.baseline.etl.processor.transform.ProcessHandle;
 import net.evecom.rd.ie.baseline.etl.processor.transform.entity.HandlerRequest;
 import net.evecom.rd.ie.baseline.etl.processor.transform.entity.HandlerResult;
+import net.evecom.rd.ie.baseline.tools.service.Page;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/process")
 @Api(tags = "解析处理模块")
 public class ProcessController {
+
+    @Resource
+    ResourceService resourceService;
 
     @ApiOperation(value = "测试解析链路", notes = "测试解析链路")
     @RequestMapping(value = "/start", method = RequestMethod.GET)
